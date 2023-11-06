7c1ae85d13bcc495f5fbd9e7a730d4cadeadfa1c
Nesgc <nestorgarciac11@gmail.com>
2023-11-05 19:02:46 -0600

    trucks view

--- app/Livewire/Units.php
+++ app/Livewire/Units.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire;
 
+use App\Models\Truck;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Units extends Component
 {
+    use WithPagination;
+
+    public $searchengine, $driver;
     public function render()
     {
-        return view('livewire.units');
+        $query = Truck::query();
+
+        if ($this->searchengine) {
+            $query = $query->where('unit', 'like', '%' . $this->searchengine . '%')
+                ->orWhere('driver', 'like', '%' . $this->searchengine . '%');
+        }
+
+        $trucks = $query->paginate(10);
+        return view('livewire.units', compact('trucks'));
     }
 }
